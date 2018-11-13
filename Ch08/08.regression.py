ff154880870d35ef60d249069d7861a6f520e84e
MachineLearningInAction <332886494@qq.com>
2018-11-13 18:40:31 +0800

    第八章部分代码

--- Ch08/08.regression.py
+++ Ch08/08.regression.py
@@ -107,6 +107,45 @@ def rssError(yArr,yHatArr):
     '''
     return ((yArr-yHatArr)**2).sum()
 
+def ridgeRegres(xMat,yMat,lam=0.2):
+    '''
+       Desc：
+           这个函数实现了给定 lambda 下的岭回归求解。
+           如果数据的特征比样本点还多，就不能再使用上面介绍的的线性回归和局部现行回归了，因为计算 (xTx)^(-1)会出现错误。
+           如果特征比样本点还多（n > m），也就是说，输入数据的矩阵x不是满秩矩阵。非满秩矩阵在求逆时会出现问题。
+           为了解决这个问题，我们下边讲一下：岭回归，这是我们要讲的第一种缩减方法。
+       Args：
+           xMat：样本的特征数据，即 feature
+           yMat：每个样本对应的类别标签，即目标变量，实际值
+           lam：引入的一个λ值，使得矩阵非奇异
+       Returns：
+           经过岭回归公式计算得到的回归系数
+    '''
+    xTx=xMat.T*xMat
+    # 岭回归就是在矩阵 xTx 上加一个 λI 从而使得矩阵非奇异，进而能对 xTx + λI 求逆
+    denom=xTx+eye(shape(xMat)[1])*lam
+    # 检查行列式是否为零，即矩阵是否可逆，行列式为0的话就不可逆，不为0的话就是可逆。
+    if linalg.det(denom)==0.0:
+        print("This matrix is singular,cannot do inverse")
+        return
+    ws = denom.I * (xMat.T*yMat)
+    return ws
+
+def ridgeTest(xArr,yArr):
+    xMat=mat(xArr);yMat=mat(yArr)
+    yMean=mean(yMat,0)
+    yMat=yMat-yMean
+    xMeans=mean(xMat,0)
+    xVar=var(xMat,0)
+    xMat=(xMat-xMeans)/xVar
+    numTestPts=30
+    wMat=zeros(numTestPts,shape(xMat)[1])
+    for i in range(numTestPts):
+        ws=ridgeRegres(xMat,yMat,exp(i-10))
+        wMat[i,:]=ws.T
+    return wMat
+
+
 def abaloneTest():
     abX,abY=loadDataSet("08.abalone.txt")
     yHat01=lwlrTest(abX[0:99],abX[0:99],abY[0:99],0.1)
